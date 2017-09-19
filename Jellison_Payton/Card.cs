8b6863ea2eb95f6ad2e36a152e13bfe21eb745e6
Payton Jellison <pcjellison@ksu.edu>
2017-09-18 22:31:44 -0500

    NOT DONE - Card Class Created - 9/18 - 10:30pm

--- Jellison_Payton/Card.cs
+++ Jellison_Payton/Card.cs
@@ -8,11 +8,57 @@ namespace Jellison_Payton
 {
     class Card
     {
-        public enum CardSuit
-        {
-            Clubs, Spades, Hearts, Diamonds
-        }
-        public CardSuit Suit { get; private set; }
+        int rank;
+        internal enum CardSuit { D, H, S, C };
+        internal CardSuit suit { get; set; }
+        bool FaceUp = true;
 
+        /// <summary>
+        /// Card constructor
+        /// </summary>
+        /// <param name="s">suit</param>
+        /// <param name="r">rank</param>
+        public Card (int s, int r)
+        {
+            suit = (Card.CardSuit)s;
+            rank = r;
+        }
+
+        /// <summary>
+        /// Checks if card is face up and if not, it returns the rank and suit of a card
+        /// </summary>
+        /// <returns>rank and suit of card</returns>
+        public override string ToString()
+        {
+            if(FaceUp == false)
+            {
+                return "XX";
+            }
+
+            if(this.rank == 1)
+            {
+                return ('A' + this.suit.ToString());
+            }
+            else if (this.rank == 10)
+            {
+                return ('T' + this.suit.ToString());
+            }
+            else if (this.rank == 11)
+            {
+                return ('J' + this.suit.ToString());
+            }
+            else if (this.rank == 12)
+            {
+                return ('Q' + this.suit.ToString());
+            }
+            else if (this.rank == 13)
+            {
+                return ('K' + this.suit.ToString());
+            }
+            else
+            {
+                return (this.rank.ToString() + this.suit.ToString());
+            }
+        }
     }
 }
